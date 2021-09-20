73588ac239bf89679e16d248b9bdbefdf9499cc3
santhiya8884 <sanammu91@gmail.com>
2021-09-20 16:23:52 +0530

    Class Rules

---
+++ SampleClassRules/SampleClassRule.java
@@ -0,0 +1,32 @@
+
+//private
+
+/*private class SampleClassRule{
+
+public static void main(String[] args){
+
+System.out.println("Welcome private Class");}
+	
+} */
+
+//Default
+/**/
+
+//protected
+
+/*protected class SampleClassRule{
+public static void main(String[] args){
+
+System.out.println("Welcome protected class");}
+	
+}*/
+
+//public
+
+
+public class SampleClassRule{
+public static void main(String[] args){
+
+System.out.println("Welcome public class");}
+	
+}
