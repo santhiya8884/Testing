4de2713315315350632bd884564f373c1e75cf36
santhiya8884 <sanammu91@gmail.com>
2021-09-27 21:05:46 +0530

    variable and constructor access in with in package and outside package

---
+++ packageparatice/Relatives1.java
@@ -0,0 +1,48 @@
+//parent class
+
+package tamilnadu.nativeplace;
+ public class Relatives1 
+{
+	 //static int number=10;
+	//private int value=10;
+	//public int value=10;
+	int value=10;
+	//protected  int value=10;
+	
+	//public constrssuctor---within package , outside the package access
+	/*int x;
+	public  Relatives1(int i){
+	x=i;
+	//this.x=i;
+	System.out.println("public"+x);
+	}*/
+	
+	
+	//private constructor-----wiht in a class
+	
+	/*private  Relatives1(int i){
+	x=i;
+	
+	System.out.println("private "+x);
+	}*/
+	
+	//protected constructor-----through extends--->inhertaince
+	
+	/*protected  Relatives1(int i){
+	x=i;
+	
+	System.out.println("protected "+x);
+	}*/
+	
+	//default contructor----within a package
+	 
+	/*Relatives1(int i){
+	x=i;
+	
+	System.out.println("default "+x);
+	}*/
+
+
+	
+	
+} 
