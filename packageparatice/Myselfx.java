89aceca51fc6a692cd248ae4572a74e9287343d7
santhiya8884 <sanammu91@gmail.com>
2021-09-27 20:57:57 +0530

    Package pratice

---
+++ packageparatice/Myselfx.java
@@ -0,0 +1,17 @@
+package tamilnadu.chennai;
+import tamilnadu.nativeplace.Relatives;
+//canonical form
+public class Myselfx 
+{
+	public static void main(String[] args){
+	
+	Relatives check=new Relatives();
+	check.doGardening();// protected---outside---extends keyword
+	check.receivePeople();//default---o/s---not not access
+	
+}
+	
+}	
+	
+	
+	
