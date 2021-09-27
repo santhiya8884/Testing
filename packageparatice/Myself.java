904a74e15030c0e6b8a2b1a8c2ff27f0c6d291a9
santhiya8884 <sanammu91@gmail.com>
2021-09-27 20:53:57 +0530

    Package pratice

---
+++ packageparatice/Myself.java
@@ -0,0 +1,24 @@
+//simple package
+
+
+
+package tamilnadu.chennai;
+
+public class Myself  
+{
+	public static void main(String[] args){
+	
+	Myself check=new Myself();
+	check.doGarding();
+	
+}
+	
+	void doGarding(){
+	System.out.println(" well garding ");
+	}
+	
+	
+}	
+	
+	
+	
---
+++ packageparatice/Relatives.java
@@ -0,0 +1,15 @@
+
+package tamilnadu.nativeplace;
+public class Relatives 
+{
+	//Default - Accesss same package- not access the Another package
+	protected  void doGardening(){   //
+		System.out.println("Gardening");
+	}
+	  void receivePeople(){ 
+		System.out.println("Reception");
+	}
+	
+	
+	
+} 
