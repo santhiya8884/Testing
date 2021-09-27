26780053038d17db2d905793d6f00990e326267e
santhiya8884 <sanammu91@gmail.com>
2021-09-27 21:12:32 +0530

    same package using extends keyword access

---
+++ packageparatice/RelativesSon.java
@@ -0,0 +1,14 @@
+package tamilnadu.nativeplace;
+public class RelativesSon extends Relatives 
+{
+public static void main(String[] args){
+	Relatives check=new Relatives();
+	check.doGardening();
+	check.receivePeople();
+
+
+}	
+	 
+	
+	
+} 
