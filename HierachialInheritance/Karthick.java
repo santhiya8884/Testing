a3403ee2529cee16559ec40b22add749eb9655a2
santhiya8884 <sanammu91@gmail.com>
2021-09-27 22:16:56 +0530

    Hierachial Inheritance

---
+++ HierachialInheritance/Karthick.java
@@ -0,0 +1,13 @@
+//Hierachial inheitance :
+
+public class Karthick extends Sivakumar{
+
+
+public static void main(String[] args){
+
+Karthick k=new Karthick();
+k.doGarding();
+
+}
+
+}
---
+++ HierachialInheritance/Surya.java
@@ -0,0 +1,13 @@
+//Hierachial inheitance :
+
+public class Surya extends Sivakumar{
+
+
+public static void main(String[] args){
+
+Surya s=new Surya();
+s.doGarding();
+
+}
+
+}
