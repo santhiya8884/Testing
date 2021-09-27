a2fcd7c731640eb84cbd08271acc1b7adb3eecfd
santhiya8884 <sanammu91@gmail.com>
2021-09-27 21:23:15 +0530

    Enacpsulation test

---
+++ Encapsulationpratice/Encaptest1.java
@@ -0,0 +1,50 @@
+package Encapsulation.test;
+public class Encaptest1{
+ 
+
+	//Example 1
+	
+	//private data 	member  
+private String name;  
+		
+//getter method fo	r name  
+/*public String getName(){  
+return name;  
+}  */
+	
+	
+//setter method for name  
+public  String    Name(String name){  
+	this.name=name; 
+	System.out.println("rrrrrrrrrrrrre"+name);
+	return name;
+	
+}  
+
+	
+	
+	//example 2
+	
+/*	//private data member  
+private String college="AKG";  
+//getter method for college  
+public String getCollege(){  
+return college;  
+}  
+public String setcollege(String college){  
+this.college=college ; 
+	return college;
+}
+*/
+	
+	//Example3
+	
+	
+/*	private String college;
+	public void setcollege(String college){
+	this.college=college;
+	}
+*/	
+
+
+}
---
+++ Encapsulationpratice/Encaptest2.java
@@ -0,0 +1,37 @@
+package Encapsulation.test;
+//import  Encapsulation.test.Encaptest1;
+public class Encaptest2 {
+public static void main(String[] args){
+
+
+//Example1	S
+	
+	//Encaptest1 s=new Encaptest1(); 
+	Encaptest2 s=new Encaptest2();
+//setting value in the name member  
+	
+	//s.name="santhiya";
+	//System.out.println(s.name);
+String result=s.Name("vijay");  
+//getting value of the name member  
+System.out.println(result);  
+} 
+
+
+	
+	//example 2
+	
+/*	Encaptest1 s=new Encaptest1(); 
+	
+	System.out.println(s.setcollege("GCES")); */
+	
+	//Example 3
+/*	Encaptest1 s=new Encaptest1();
+	System.out.println(s.setcollege());
+	System.out.println(s.college);
+	
+	
+}	
+*/
+
+}
