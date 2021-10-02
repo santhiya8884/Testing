6b2f0eb83b0b8e9713c11bbf2958937c3f3959a7
santhiya8884 <sanammu91@gmail.com>
2021-10-02 09:03:12 +0530

    Interface basic program using implement keyword

---
+++ Interface/Samplex.java
@@ -0,0 +1,19 @@
+//interface 2:
+interface Samplex{  
+	static final  int a=10;
+	int b=9;
+	/*final c=8;
+	private d=7;
+	default f =6;
+	public g=5;
+	protected i=4;*/////Error
+	int c=8;
+	int d=7;
+	int f =6;
+	int g=5;
+	int i=4;
+public void SevenDaysWork();
+public void OpenTimeNineThirty();
+public void CloseTimeNineThirty();
+	
+}
---
+++ Interface/WorkSample.java
@@ -0,0 +1,38 @@
+
+public class WorkSample implements Samplex { //Employeee
+public static void main(String[] args){ 
+
+WorkSample employee=new WorkSample();
+employee.SevenDaysWork();
+/*System.out.println(Samplex.a);
+System.out.println(Samplex.b);
+System.out.println(Samplex.c);
+System.out.println(Samplex.d);
+System.out.println(Samplex.f);
+System.out.println(Samplex.g);
+System.out.println(Samplex.i);*/////interfece name called..
+/*System.out.println(employee.a);
+System.out.println(employee.b);
+System.out.println(employee.c);
+System.out.println(employee.d);
+System.out.println(employee.f);
+System.out.println(employee.g);
+System.out.println(employee.i);*//////implemnts using object name accesseddd...
+	
+//employee.OpenTimeNineThirty();
+//employee.CloseTimeNineThirty();
+
+	/*Samplex.SevenDaysWork();
+Samplex.OpenTimeNineThirty();
+Samplex.CloseTimeNineThirty();*/
+}
+public void SevenDaysWork(){
+System.out.println("Okay");
+}
+public void OpenTimeNineThirty(){
+System.out.println("Okay");
+}
+public void CloseTimeNineThirty(){
+System.out.println("Okay");
+}
+}
