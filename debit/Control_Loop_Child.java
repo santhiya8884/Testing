ffd2209379601f7b5e9e5d814e488fbd73b8b7f8
santhiya8884 <sanammu91@gmail.com>
2021-10-07 23:49:49 +0530

    Looping while1 practice

--- debit/Control_Loop_Child.java
+++ debit/Control_Loop_Child.java
@@ -11,6 +11,13 @@ public class Control_Loop_Child extends Control_Looping_Demo {
 
 	public  void while1() {
 		// TODO Auto-generated method stub
+		////Question : print-----1 4 7 10 13
+		int i=0,no=1;
+		while (i<5) {
+			System.out.print(no+" ");
+			no=no+3;
+			i=i+1;
+		}
 		
 	}
 
