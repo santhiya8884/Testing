9bff7d33ecb60632316436761462fc9e0c462b84
santhiya8884 <sanammu91@gmail.com>
2021-10-07 23:06:02 +0530

    Looping practice

---
+++ debit/Control_Looping_Demo.java
@@ -0,0 +1,29 @@
+package bank.icici.debit;
+
+public class Control_Looping_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Control_Looping_Demo sara=new Control_Looping_Demo();
+		sara.doWhileLoopDemo();
+		sara.whileLoopDemo();
+		sara.forLoopDemo();
+		
+	}
+
+	public void whileLoopDemo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void forLoopDemo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void doWhileLoopDemo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
