ed88aa771554702a5e57347beceac57decaf9ca4
santhiya8884 <sanammu91@gmail.com>
2021-10-07 23:35:44 +0530

    Looping while practice

---
+++ debit/Control_Loop_Child.java
@@ -0,0 +1,17 @@
+package bank.icici.debit;
+
+public class Control_Loop_Child extends Control_Looping_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Control_Loop_Child sara=new Control_Loop_Child();
+		sara.while1();
+		
+	}
+
+	public  void while1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
