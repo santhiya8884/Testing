404602a86827950174dc3f7300c9d0c3faacc01e
santhiya8884 <sanammu91@gmail.com>
2021-10-07 22:42:32 +0530

    IF Demo1

---
+++ debit/Condition_Statements_Demo.java
@@ -0,0 +1,56 @@
+package bank.icici.debit;
+
+public class Condition_Statements_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Condition_Statements_Demo sara=new Condition_Statements_Demo();
+		sara.ifdemo();
+		sara.ifdemo1();
+		sara.ifelse();
+		sara.elseif();
+		sara.nestedif();
+
+	}
+
+	public void elseif() {
+		// TODO Auto-generated method stub
+		//
+		
+	}
+
+	public void nestedif() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void ifelse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void ifdemo() {
+		// TODO Auto-generated method stub
+		//car parking....
+		String vechicle="Two Wheeler";
+		String customer="4 wheeler";
+		if (customer==vechicle) {
+			System.out.println("Park inside office");
+		}
+		
+		
+	}
+	public void ifdemo1() {
+		// TODO Auto-generated method stub
+		//menu item checking...
+		String manuitem="biriyani";
+		String Cutsomerchoice="biriyani";
+		if (manuitem==Cutsomerchoice) {
+			System.out.println("Prepared biriyani");
+		}
+		
+		
+	}
+	
+
+}
