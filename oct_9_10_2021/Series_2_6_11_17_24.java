2aa9fe2a9aaca1f6c21760796d62ed4b47b974d6
santhiya8884 <sanammu91@gmail.com>
2021-10-09 22:50:30 +0530

    Series practice

---
+++ oct_9_10_2021/Series_2_6_11_17_24.java
@@ -0,0 +1,13 @@
+
+package oct_9_10_2021;
+
+public class Series_2_6_11_17_24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//2_6_11_17_24
+		//2+4=6, 6+5=11, 11+6=17, 17+7=24
+
+	}
+
+}
---
+++ oct_9_10_2021/Series_pra_1_3_4_8.java
@@ -0,0 +1,50 @@
+package oct_9_10_2021;
+
+public class Series_pra_1_3_4_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1_3-4_8_15_27----Output
+		//1+3+4=8, 3+4+8=15, 4+8+15=27
+		
+		Series_pra_1_3_4_8 sara=new Series_pra_1_3_4_8();
+		sara.pra_1_3_4_8_15_27();
+	}
+
+	public void pra_1_3_4_8_15_27() {
+		// TODO Auto-generated method stub
+		int i=1,j=3,k=4,n,sum;
+		System.out.print(1+" ");
+		System.out.print(3+" ");
+		System.out.print(4+" ");
+		while (k<27) {
+			sum=i+j+k;
+			System.out.print(sum+" ");
+			i=j;
+			j=k;
+			k=sum;
+			//n=i+j+k;
+		}
+		/*n=i+j+k;
+		System.out.print(n+" ");
+		i=j;
+		j=k;
+		k=n;
+		n=i+j+k;
+		System.out.print(n+" ");
+		i=j;
+		j=k;
+		k=n;
+		n=i+j+k;
+		System.out.print(n+" ");*/
+		
+		
+		
+		
+		/*System.out.print(15+" ");
+		System.out.print(27+" ");*/
+		
+	}
+	
+
+}
