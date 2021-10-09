15aaf438f4ba7198f2b00b7faef7adc2fb4185b9
santhiya8884 <sanammu91@gmail.com>
2021-10-09 23:01:01 +0530

    Series practice

--- oct_9_10_2021/Series_2_6_11_17_24.java
+++ oct_9_10_2021/Series_2_6_11_17_24.java
@@ -7,7 +7,33 @@ public class Series_2_6_11_17_24 {
 		// TODO Auto-generated method stub
 		//2_6_11_17_24
 		//2+4=6, 6+5=11, 11+6=17, 17+7=24
+		Series_2_6_11_17_24 sara=new Series_2_6_11_17_24();
+		sara.pra2_6_11_17_24();
 
+	}
+
+	public void pra2_6_11_17_24() {
+		// TODO Auto-generated method stub
+		int i=2,j=4;
+		
+		while (i<=24) {
+			System.out.print(i+" ");
+			i=i+j;
+			j++;
+		}
+		
+		/*System.out.print(i+" ");
+		i=i+j;
+		j++;
+		System.out.print(i+" ");
+		i=i+j;
+		j++;
+		System.out.print(i+" ");
+		System.out.print(17+" ");
+		System.out.print(24+" ");*/
+
+		
+		
 	}
 
 }
