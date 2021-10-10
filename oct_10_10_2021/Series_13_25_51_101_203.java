3bd914c2ff92fd473aaf5d2355678a85e5c0d232
santhiya8884 <sanammu91@gmail.com>
2021-10-10 07:24:33 +0530

    13_25_51_101_203

---
+++ oct_10_10_2021/Series_13_25_51_101_203.java
@@ -0,0 +1,25 @@
+package oct_10_10_2021;
+
+public class Series_13_25_51_101_203{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Series_13_25_51_101_203 sara=new Series_13_25_51_101_203();
+		sara.prac_13_25_51_101_203();
+
+	}
+
+	public void prac_13_25_51_101_203() {
+		// TODO Auto-generated method stub
+		//13 25 51 101 203
+		System.out.print(13+" ");
+		System.out.print(25+" ");
+		System.out.print(51+" ");
+		System.out.print(101+" ");
+		System.out.print(203+" ");
+
+		
+	}
+
+}
+//output : 13 25 51 101 203
