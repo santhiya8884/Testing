87e0a4d8d4117abb97a2b105b9a83eb193c8f7be
santhiya8884 <sanammu91@gmail.com>
2021-10-11 23:34:10 +0530

    21 9 21 11 21 13 21 15 Series

---
+++ oct_11_10_2021/Series_21_9_21_1_21_13_21_15.java
@@ -0,0 +1,40 @@
+package oct_11_10_2021;
+
+public class Series_21_9_21_1_21_13_21_15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Series_21_9_21_1_21_13_21_15 sara=new Series_21_9_21_1_21_13_21_15();
+		sara.pract_21_9_21_1_21_13_21_15();
+	}
+	public void pract_21_9_21_1_21_13_21_15(){ 
+		int n=21,n1=9;
+		while (n1<=15) {
+			System.out.print(n+" ");
+			System.out.print(n1+" ");
+			n1=n1+2;
+		}
+		/*System.out.print(n+" ");
+		System.out.print(n1+" ");
+		n1=n1+2;
+		System.out.print(n+" ");
+		System.out.print(n1+" ");
+		n1=n1+2;
+		System.out.print(n+" ");
+		System.out.print(n1+" ");
+		n1=n1+2;
+		System.out.print(n+" ");
+		System.out.print(n1+" ");
+		n1=n1+2;*/
+		
+		/*System.out.print(21+" ");
+		System.out.print(9+" ");
+		System.out.print(21+" ");
+		System.out.print(11+" ");
+		System.out.print(21+" ");
+		System.out.print(13+" ");
+		System.out.print(21+" ");
+		System.out.print(15+" ");*/
+	}
+}
+//output : 21 9 21 11 21 13 21 15 
