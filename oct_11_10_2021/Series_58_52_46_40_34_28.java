e88817a83d9e17816a3a08045328e04cf3cea09c
santhiya8884 <sanammu91@gmail.com>
2021-10-11 23:46:49 +0530

    58 52 46 40 34  Series

---
+++ oct_11_10_2021/Series_58_52_46_40_34_28.java
@@ -0,0 +1,38 @@
+package oct_11_10_2021;
+
+public class Series_58_52_46_40_34_28 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Series_58_52_46_40_34_28 sara=new Series_58_52_46_40_34_28();
+		sara.pract_58_52_46_40_34_28();
+	}
+	public void pract_58_52_46_40_34_28() { 
+		int n=58;
+		while (n>=28) {
+			System.out.print(n+" ");
+			n=n-6;
+		}
+		/*System.out.print(n+" ");
+		n=n-6;
+		System.out.print(n+" ");
+		n=n-6;
+		System.out.print(n+" ");
+		n=n-6;
+		System.out.print(n+" ");
+		n=n-6;
+		System.out.print(n+" ");
+		n=n-6;
+		System.out.print(n+" ");*/
+		
+		/*System.out.print(58+" ");
+		System.out.print(52+" ");
+		System.out.print(46+" ");
+		System.out.print(40+" ");
+		System.out.print(34+" ");
+		System.out.print(32+" ");
+		;
+		*/
+	}
+}
+//output : 58 52 46 40 34 28 
