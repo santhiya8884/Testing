dbe53e3c3f62cf931273363de42dc755fb6f9551
santhiya8884 <sanammu91@gmail.com>
2021-10-11 22:46:03 +0530

    36 34 30 28 24 The next term of this  Series

---
+++ oct_11_10_2021/Series_36_34_30_28_24_22.java
@@ -0,0 +1,44 @@
+package oct_11_10_2021;
+
+public class Series_36_34_30_28_24_22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Series_36_34_30_28_24_22 sara=new Series_36_34_30_28_24_22();
+		sara.pract_36_34_30_28_24_22();
+	}
+	public void pract_36_34_30_28_24_22() { 
+		//36 34 32 30 28 24  what shoud be the next????????--->22
+		int n=36;
+		while (n>=22) {
+			if(n==22) {
+				System.out.print(n+" ");
+			}
+			n=n-2;
+		}
+		/*System.out.print(n+" ");
+		n=n-2;
+		System.out.print(n+" ");
+		n=n-4;
+		System.out.print(n+" ");
+		n=n-2;
+		System.out.print(n+" ");
+		n=n-4;
+		System.out.print(n+" ");
+		n=n-2;
+		System.out.print(n+" ");*/
+		
+		/*System.out.print(36+" ");
+		System.out.print(34+" ");
+		System.out.print(32+" ");
+		System.out.print(30+" ");
+		System.out.print(28+" ");
+		System.out.print(24+" ");
+		System.out.print(22+" ");
+		*/
+		
+		
+	}
+
+}
+
