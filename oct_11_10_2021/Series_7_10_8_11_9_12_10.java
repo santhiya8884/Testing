f2d1122bb9c0c82b63348b66b4e68fb299e755fa
santhiya8884 <sanammu91@gmail.com>
2021-10-11 22:29:17 +0530

    7 10 8 11 9 12 10 Series

---
+++ oct_11_10_2021/Series_7_10_8_11_9_12_10.java
@@ -0,0 +1,46 @@
+package oct_11_10_2021;
+
+public class Series_7_10_8_11_9_12_10{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Series_7_10_8_11_9_12_10 sara=new Series_7_10_8_11_9_12_10();
+		sara.prac_7_10_8_11_9_12_10();
+	}
+	public void prac_7_10_8_11_9_12_10() { 
+		int n1=7,n2=10;
+		while (n1<=10){
+			System.out.print(n1+" ");
+			if(n1>=10) {
+				break;
+			}
+			System.out.print(n2+" ");
+			n1=n1+1;
+			n2=n2+1;
+		}
+		/*System.out.print(n1+" ");
+		System.out.print(n2+" ");
+		n1=n1+1;
+		n2=n2+1;
+		System.out.print(n1+" ");
+		System.out.print(n2+" ");
+		n1=n1+1;
+		n2=n2+1;
+		System.out.print(n1+" ");
+		System.out.print(n2+" ");
+		n1=n1+1;
+		n2=n2+1;
+		System.out.print(n1+" ");
+		System.out.print(n2+" ");
+		*/
+		/*System.out.print(7+" ");
+		System.out.print(10+" ");
+		System.out.print(8+" ");
+		System.out.print(11+" ");
+		System.out.print(9+" ");
+		System.out.print(12+" ");
+		System.out.print(10+" ");*/
+	}
+
+}
+//output : 7 10 8 11 9 12 10 
