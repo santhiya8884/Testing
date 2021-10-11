086b8d4ca7b06156427168e3b1b0e1d90f50f903
santhiya8884 <sanammu91@gmail.com>
2021-10-11 22:58:25 +0530

    22 21 23 22 24 24 25  Series

---
+++ oct_11_10_2021/Series_22_21_23_22_24_23_25.java
@@ -0,0 +1,46 @@
+package oct_11_10_2021;
+
+public class Series_22_21_23_22_24_23_25{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Series_22_21_23_22_24_23_25 sara=new Series_22_21_23_22_24_23_25();
+		sara.prac_22_21_23_22_24_23_25();
+	}
+	public void prac_22_21_23_22_24_23_25() { 
+		int n1=22,n2=21;
+		while (n1<=25){
+			System.out.print(n1+" ");
+			if(n1>=25) {
+				break;
+			}
+			System.out.print(n2+" ");
+			n1=n1+1;
+			n2=n2+1;
+		}
+		/*System.out.print(n1+" ");
+		System.out.print(n2+" ");
+		n1=n1+1;
+		n2=n2+1;
+		System.out.print(n1+" ");
+		System.out.print(n2+" ");
+		n1=n1+1;
+		n2=n2+1;
+		System.out.print(n1+" ");
+		System.out.print(n2+" ");
+		n1=n1+1;
+		n2=n2+1;
+		System.out.print(n1+" ");
+		System.out.print(n2+" ");
+		*/
+		/*System.out.print(22+" ");
+		System.out.print(21+" ");
+		System.out.print23+" ");
+		System.out.print(22+" ");
+		System.out.print(24+" ");
+		System.out.print(23+" ");
+		System.out.print(25+" ");*/
+	}
+
+}
+//output : 22 21 23 22 24 23 25 
