f2d9c588bbf38faeefd6f43bcc37a52437b10e01
santhiya8884 <sanammu91@gmail.com>
2021-10-11 23:17:42 +0530

    53 53 40 40 27 27 14Series

---
+++ oct_11_10_2021/Series_53_53_40_40_27_27_14.java
@@ -0,0 +1,45 @@
+
+package oct_11_10_2021;
+
+public class Series_53_53_40_40_27_27_14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Series_53_53_40_40_27_27_14 sara=new Series_53_53_40_40_27_27_14();
+		sara.prat_53_53_40_40_27_27_14();
+	}
+	public void prat_53_53_40_40_27_27_14() { 
+		int n1=53;
+		while (n1>=14) {
+			System.out.print(n1+" ");
+			if (n1==14) {
+				break;
+			}
+			System.out.print(n1+" ");
+			n1=n1-13;
+		}
+		/*System.out.print(n1+" ");
+		System.out.print(n1+" ");
+		n1=n1-13;
+		System.out.print(n1+" ");
+		System.out.print(n1+" ");
+		n1=n1-13;
+		System.out.print(n1+" ");
+		System.out.print(n1+" ");
+		n1=n1-13;
+		System.out.print(n1+" ");
+		System.out.print(n1+" ");
+		n1=n1-13;
+		
+		/*System.out.print(53+" ");
+		System.out.print(53+" ");
+		System.out.print40+" ");
+		System.out.print(40+" ");
+		System.out.print(27+" ");
+		System.out.print(27+" ");
+		System.out.print(14+" ");*/
+
+	}
+	
+}
+//output : 53 53 40 40 27 27 14 
