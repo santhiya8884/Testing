d7babbc105180584eaf06f96700b4597f0ce1168
santhiya8884 <sanammu91@gmail.com>
2021-10-12 17:21:03 +0530

    Variable practice

---
+++ oct_12_10_2021/Variable_practice.java
@@ -0,0 +1,24 @@
+package oct_12_10_2021;
+
+public class Variable_practice {
+	static int value=100;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//System.out.println(a);	//---Error
+		int c=70;
+		System.out.println(value);	
+		Variable_practice sara=new Variable_practice();
+		sara.add();
+	}
+	//System.out.println(c);	
+		
+
+	void add() { //method
+		int a=5,b=10;
+		System.out.println(value);
+		System.out.println(a+b);	
+	}
+	
+	//System.out.println(a);	//Error
+
+}
