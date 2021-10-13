dcf43f6137458a2b71971d4af9ef90922822d419
santhiya8884 <sanammu91@gmail.com>
2021-10-13 22:45:55 +0530

    print the factorial of the given number

---
+++ oct_13/Factorial_Program.java
@@ -0,0 +1,53 @@
+package oct_13;
+
+public class Factorial_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Factorial_Program sara=new Factorial_Program();
+		sara.factorial();
+	}
+
+	public void factorial() {
+		// TODO Auto-generated method stub
+			int n=5,mul=1;
+			while (n>=1) {
+				mul=mul*n;
+				//System.out.println(mul);
+				n=n-1;
+			}
+			System.out.println(mul);
+			
+	
+		
+		
+		/*while (n<=5) {
+			mul=mul*n;
+			//System.out.println(mul);
+			n=n+1;
+		}*/
+		//System.out.println(mul);
+		/*System.out.println(mul);
+		mul=mul*n;
+		n=n+1;
+		System.out.println(mul);
+		mul=mul*n;
+		n=n+1;
+		System.out.println(mul);
+		mul=mul*n;
+		n=n+1;
+		System.out.println(mul);
+		mul=mul*n;
+		n=n+1;
+		System.out.println(mul);
+		mul=mul*n;
+		n=n+1;
+		System.out.println(mul);
+		mul=mul*n;
+		n=n+1;*/
+
+		
+		
+	}
+
+}
