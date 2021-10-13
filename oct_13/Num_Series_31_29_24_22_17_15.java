0709a58cfdd84d246d91dcbe01b2c45ac3430ec3
santhiya8884 <sanammu91@gmail.com>
2021-10-13 23:53:01 +0530

    Numebr series 31 29 24 22 17 15

---
+++ oct_13/Num_Series_31_29_24_22_17_15.java
@@ -0,0 +1,41 @@
+package oct_13;
+
+public class Num_Series_31_29_24_22_17_15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Num_Series_31_29_24_22_17_15 sara=new Num_Series_31_29_24_22_17_15();
+		sara.Series();
+	}
+
+	public void Series() {
+		// TODO Auto-generated method stub
+		int n=31;
+		while (n>=15) {
+			System.out.print(n+" ");
+			n=n-2;
+			System.out.print(n+" ");
+			n=n-5;
+		}
+		/*System.out.println(n);
+		
+		n=n-2;
+		System.out.println(n);
+		n=n-5;
+		System.out.println(n);
+		n=n-2;
+		System.out.println(n);
+		n=n-5;
+		System.out.println(n);*/
+		
+		/*System.out.println(31);
+		System.out.println(29);
+		System.out.println(24);
+		System.out.println(22);
+		System.out.println(17);
+		System.out.println(15);*/
+		
+	}
+
+}
+//output :31 29 24 22 17 15 
