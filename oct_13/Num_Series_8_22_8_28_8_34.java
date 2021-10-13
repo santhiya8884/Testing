4afe02ddb38b3d24bafd7aedcd4c6509b5326599
santhiya8884 <sanammu91@gmail.com>
2021-10-13 23:42:08 +0530

    Numebr series 8 22 28 8 34

---
+++ oct_13/Num_Series_8_22_8_28_8_34.java
@@ -0,0 +1,43 @@
+package oct_13;
+
+public class Num_Series_8_22_8_28_8_34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Num_Series_8_22_8_28_8_34 sara=new Num_Series_8_22_8_28_8_34();
+		sara.Series();
+
+	}
+
+	public  void Series() {
+		// TODO Auto-generated method stub
+		int n=8;
+		int m=22;
+		while (m<=34) {
+			System.out.print(n+" ");
+			System.out.print(m+" ");
+			m=m+6;
+		}
+		/*System.out.println(n);
+		System.out.println(m);
+		m=m+6;
+		System.out.println(n);
+		System.out.println(m);
+		m=m+6;
+		System.out.println(n);
+		System.out.println(m);
+		m=m+6;*/
+		
+		
+		/*System.out.println(8);
+		System.out.println(22);
+		System.out.println(8);
+		System.out.println(28);
+		System.out.println(8);
+		System.out.println(34);*/
+		
+		
+		
+	}
+
+}
