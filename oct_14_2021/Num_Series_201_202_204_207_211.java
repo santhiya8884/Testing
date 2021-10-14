6b2961cbb3d0d970f1da7d952fe8a6e862110017
santhiya8884 <sanammu91@gmail.com>
2021-10-14 09:31:55 +0530

    Number Series 201 202 204 207 211

---
+++ oct_14_2021/Num_Series_201_202_204_207_211.java
@@ -0,0 +1,37 @@
+package oct_14_2021;
+
+public class Num_Series_201_202_204_207_211 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Num_Series_201_202_204_207_211 sara= new Num_Series_201_202_204_207_211();
+		sara.series();
+	}
+
+	public void series() {
+		// TODO Auto-generated method stub
+		//201 202 204 207 211
+		int n=201,i=1;
+		while (i<=5) {
+			System.out.print(n+" ");
+			n=n+i;
+			i++;
+		}
+		/*System.out.println(n);
+		n=n+i;
+		i++;
+		System.out.println(n);
+		n=n+i;
+		i++;
+		System.out.println(n);
+		n=n+i;
+		i++;
+		System.out.println(n);
+		n=n+i;
+		i++;
+		System.out.println(n);
+		n=n+i;
+		i++;*/
+	}
+
+}
