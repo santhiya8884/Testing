f175479df3beaebbc3c4c4d23f80c1d388c60f0c
santhiya8884 <sanammu91@gmail.com>
2021-10-14 09:44:27 +0530

    Number Series 544 509 474 439 404

---
+++ oct_14_2021/Num_Series_544_509_474_439_404.java
@@ -0,0 +1,34 @@
+package oct_14_2021;
+
+public class Num_Series_544_509_474_439_404 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Num_Series_544_509_474_439_404 sara= new Num_Series_544_509_474_439_404();
+		sara.Series();
+		
+	}
+
+	public void Series() {
+		// TODO Auto-generated method stub
+		//544 509 474 439 404
+		int n=544,count=0;
+		while (count<=4) {
+			System.out.print(n+" ");
+			n=n-35;
+			count++;
+		}
+		/*System.out.print(n+" ");
+		n=n-35;
+		System.out.print(n+" ");
+		n=n-35;
+		System.out.print(n+" ");
+		n=n-35;
+		System.out.print(n+" ");
+		n=n-35;
+		System.out.print(n+" ");
+		n=n-35;*/
+	}
+
+}
+//output :544 509 474 439 404 
