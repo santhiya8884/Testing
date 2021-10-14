e98365af3124fe3fbca71e05eeae93a54b86ea39
santhiya8884 <sanammu91@gmail.com>
2021-10-14 21:53:03 +0530

    Logical AND Bitwise AND ,Comparision operator

---
+++ oct_14_2021/Logical_Operator_AND_Bitwise_AND.java
@@ -0,0 +1,23 @@
+package oct_14_2021;
+
+public class Logical_Operator_AND_Bitwise_AND {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=12,b=6;
+		//logical AND-----First condition false no check for the second condition, result is false
+		//Bitwise AND----- Whether First condition true or false  check for the second condition,
+		System.out.println((x<b)&&(x>b));//logical AND
+		System.out.println((x>b)&(x>b));//Bitwise AND
+		System.out.println((x>b)&&(x<b));//logical AND
+		System.out.println((b<x)&(x>b));//Bitwise AND
+
+	}
+
+}
+/* output: 
+ * false
+true
+false
+true
+*/
