7b74fe47479ab2ed0a9068414e82a1f6490a5503
santhiya8884 <sanammu91@gmail.com>
2021-10-14 20:52:51 +0530

    Unary Operator practic using  2-operand Negating expression

---
+++ oct_14_2021/Unary_Operator_2Operand_negating_Expession.java
@@ -0,0 +1,25 @@
+package oct_14_2021;
+
+public class Unary_Operator_2Operand_negating_Expession{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=50;
+		System.out.println("Unary operator with 2operand-----negating expression");
+		System.out.println(a++ + b--);
+		System.out.println(++a + --a);
+		System.out.println(++a + b--);
+		System.out.println(a*2 + a--);
+		System.out.println(a++ + a/5);
+
+	}
+
+}
+/* output :
+ * Unary operator with 2operand -----negating expression
+60
+23
+61
+36
+13
+*/
