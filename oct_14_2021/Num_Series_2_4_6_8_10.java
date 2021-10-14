a7d7ff1de84d5d94516b9a5391eca6383df97088
santhiya8884 <sanammu91@gmail.com>
2021-10-14 09:25:37 +0530

    Number Series 2 4 6 8 10

---
+++ oct_14_2021/Num_Series_2_4_6_8_10.java
@@ -0,0 +1,31 @@
+package oct_14_2021;
+
+public class Num_Series_2_4_6_8_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Num_Series_2_4_6_8_10 sara=new Num_Series_2_4_6_8_10();
+		sara.Series();
+	}
+
+	public void Series() {
+		// TODO Auto-generated method stub
+		int n=2;
+		while (n<=10) {
+			System.out.print(n+" ");
+			n=n+2;
+		}
+	/*	System.out.println(n);
+		n=n+2;
+		System.out.println(n);
+		n=n+2;
+		System.out.println(n);
+		n=n+2;
+		System.out.println(n);
+		n=n+2;
+		System.out.println(n);
+		n=n+2;*/
+	}
+
+}
+//output : 2 4 6 8 10 
