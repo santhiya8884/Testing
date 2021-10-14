f1f9b93a01f8d7518d86f99db3e8a94cbe2c33ca
santhiya8884 <sanammu91@gmail.com>
2021-10-14 09:02:06 +0530

    Number Series 1.5 2.3 3.1 3.9 4.7

---
+++ oct_14_2021/Num_Series.java
@@ -0,0 +1,28 @@
+package oct_14_2021;
+
+public class Num_Series {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1.5 2.3 3.1 3.9 
+		float n=1.5f,count=0;
+		while (count<5) {
+			System.out.print(String.format("%.1f",n)+" ");
+			n=n+0.8f;
+			count++;
+		}
+		
+		/*System.out.print(String.format("%.1f",n)+" ");
+		n=n+0.8f;
+		System.out.print(String.format("%.1f",n)+" ");
+		n=n+0.8f;
+		System.out.print(String.format("%.1f",n)+" ");
+		n=n+0.8f;
+		System.out.print(String.format("%.1f",n)+" ");
+		n=n+0.8f;
+		System.out.print(String.format("%.1f",n)+" ");*/
+		
+
+	}
+
+}
