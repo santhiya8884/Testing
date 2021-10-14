73ae6d0613c2280449f5f5e732e6610e79e8b25d
santhiya8884 <sanammu91@gmail.com>
2021-10-14 21:22:15 +0530

    Arithmetic Operator Example

---
+++ oct_14_2021/Arithmetic_Operators_Example.java
@@ -0,0 +1,25 @@
+package oct_14_2021;
+
+public class Arithmetic_Operators_Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=50,y=3;
+		System.out.println("Arithmetic operator Example");
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+	}
+
+}
+/*output :
+ * Arithmetic operator Example
+53
+47
+150
+16
+2
+*/
+ 
