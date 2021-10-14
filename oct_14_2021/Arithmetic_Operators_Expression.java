dd466815b72af780157a81c806e5c5e8a1802bec
santhiya8884 <sanammu91@gmail.com>
2021-10-14 21:34:53 +0530

    Arithmetic Operator Example using Expression

---
+++ oct_14_2021/Arithmetic_Operators_Expression.java
@@ -0,0 +1,24 @@
+package oct_14_2021;
+
+public class Arithmetic_Operators_Expression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5,y=19,z=4;
+		System.out.println("Arthimetic Operators using Expression");
+		System.out.println(z%x/y);
+	//	System.out.println(x/y);
+		//System.out.println(z%0);
+		System.out.println(x+y*z%x/y-z*x);
+		System.out.println(x+y*z%x/y-z*x);
+		System.out.println(x+y*z%x/y-z*x);
+	}
+
+}
+/* output :
+ * Arthimetic Operators using Expression
+0
+-15
+-15
+-15 
+*/
