e9c30924399fd207b6eff79bbdd6d9651edbf0df
santhiya8884 <sanammu91@gmail.com>
2021-10-14 21:03:41 +0530

    Unary Operator practic using negating Expression and inveting the boolean value

---
+++ oct_14_2021/Unary_Invert_Boolean.java
@@ -0,0 +1,27 @@
+package oct_14_2021;
+
+public class Unary_Invert_Boolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=-40;
+		boolean x=true,y=false;
+		/*System.out.println(~a);
+		System.out.println(~b);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(~a+b);
+		System.out.println(~(a+b));*/
+		System.out.println(!x);
+		System.out.println(!y);
+		System.out.println(!x);
+		System.out.println(!(!x));
+	}
+
+}
+/*output :
+ * false
+true
+false
+true
+*/
