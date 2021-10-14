d4a48362055cd6439a51b223ca4b309edeb86fd7
santhiya8884 <sanammu91@gmail.com>
2021-10-14 20:43:55 +0530

    Unary Operator practic using 1-operand Increment/Decrement

---
+++ oct_14_2021/Unary_Operator_Inc_Dec_Ex1.java
@@ -0,0 +1,32 @@
+package oct_14_2021;
+
+public class Unary_Operator_Inc_Dec_Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Unary operator with 1-operand
+		int a=100;
+		System.out.println(a++);
+		System.out.println(a--);
+		System.out.println(--a);
+		System.out.println(a--);
+		System.out.println(a*2);
+		System.out.println(2*a);
+		System.out.println(a/2);
+		System.out.println(2%a);
+		System.out.println(a*2);
+
+	}
+
+}
+//output:
+/*100
+101
+99
+99
+196
+196
+49
+2
+196
+*/
