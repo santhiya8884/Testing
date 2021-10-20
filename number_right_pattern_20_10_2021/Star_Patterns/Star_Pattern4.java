904b35bef9592b40bbc521b61de561527e17ea2e
santhiya8884 <sanammu91@gmail.com>
2021-10-20 23:40:58 +0530

    Star pattern 4

---
+++ number_right_pattern_20_10_2021/Star_Patterns/Star_Pattern4.java
@@ -0,0 +1,34 @@
+package number_right_pattern_20_10_2021;
+
+public class Star_Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		    * 
+	      * * 
+	    * * * 
+	  * * * * 
+	* * * * * 
+	*/ 
+
+		for(int row=1;row<=5;row++) { 
+			for(int col=4;col>=row;col--) { 
+				System.out.print(" "+" ");
+			}
+			for(int col=1;col<=row;col++) { 
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+/* output: 
+ 		* 
+      * * 
+    * * * 
+  * * * * 
+* * * * * 
+* */
+
