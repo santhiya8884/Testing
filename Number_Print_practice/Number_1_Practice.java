46a473bf89f8ee4856f430ebb6fd4e080f118738
santhiya8884 <sanammu91@gmail.com>
2021-10-23 16:02:13 +0530

    Number 1 pratcie

---
+++ Number_Print_practice/Number_1_Practice.java
@@ -0,0 +1,35 @@
+package number_printing_practice_23_10_2021;
+
+
+
+public class Number_1_Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Number_1_Practice sara=new Number_1_Practice();
+		sara.letter_5();
+
+
+	}
+
+	public void letter_5() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=7;row++) {
+			System.out.println("*");
+		}
+		
+	}
+
+}
+/* output :
+
+
+*
+*
+*
+*
+*
+*
+*
+
+*/
