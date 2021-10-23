6f9963e5167493ddadb9d00217265c3112f4a84d
santhiya8884 <sanammu91@gmail.com>
2021-10-23 14:47:18 +0530

    D letter pratice

---
+++ Name_letters_pattern_practice_21_10_2021/Letter_D_practice.java
@@ -0,0 +1,61 @@
+package letters_pattern_practice_21_10_2021;
+
+public class Letter_D_practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Letter_D_practice sara=new Letter_D_practice();
+		sara.e_Letter();
+		
+
+	}
+
+	public void e_Letter() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=7;row++) { 
+		if(row==1||row==7) { 
+			for(int col=1;col<=5;col++) { 
+				System.out.print("*"+" ");
+			}
+		}
+		else { 
+			for(int col=1;col<=5;col++) {
+				if(col==1||col==5) {
+					System.out.print("*"+" ");
+				}
+				
+				else { 
+					System.out.print("   ");
+				}
+				
+			}
+			/*int col=1;
+			if(col==1) {
+				System.out.print("*"+"   ");
+			}
+			col++;
+			if(col==2) {
+				System.out.print("  ");
+			}
+			col++;
+			if(col==3) {
+				System.out.print("  ");
+			}
+			col++;
+			if(col==4) {
+				System.out.print("  ");
+			}
+			col++;
+			if(col==5) {
+				System.out.print("*"+"  ");
+			}*/
+			
+		/*	if(row==2||row==3|row==4||row==5||row==6) { 
+				System.out.print("*"+"       *");
+			}*/
+		}
+		System.out.println();
+		}
+	}
+
+}
