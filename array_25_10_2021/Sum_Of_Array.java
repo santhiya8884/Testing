da2d7e555a17929b586b3c5633af923afcad8c4a
santhiya8884 <sanammu91@gmail.com>
2021-10-25 14:52:20 +0530

    Sum of array elements

---
+++ array_25_10_2021/Sum_Of_Array.java
@@ -0,0 +1,16 @@
+package array_25_10_2021;
+
+public class Sum_Of_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sum_Of_Array sara=new Sum_Of_Array();
+		sara.sum_Of_Array_Elements();
+	}
+
+	public void sum_Of_Array_Elements() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
