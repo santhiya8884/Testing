ef4a5c0ab2ccec8e32336d84731e0fcd19af1953
santhiya8884 <sanammu91@gmail.com>
2021-11-02 14:57:13 +0530

    Unique  character

---
+++ String_Practice_01_11_2021/Unique_Characters_Print.java
@@ -0,0 +1,58 @@
+package string_01_11_2021;
+
+public class Unique_Characters_Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Unique_Characters_Print sara=new Unique_Characters_Print();
+		sara.UniqueCharcter();
+	}
+	public void UniqueCharcter() {
+		// TODO Auto-generated method stub
+		String name="RANJAN BABU";
+		char[] letters=new char[name.length()];
+		int [] freq=new int[name.length()];
+		//count of each character :
+		
+		for(int i=0;i<name.length();i++) { 
+			//char ch=name.charAt(i);
+			//letters[i]=ch;
+			int count=0;
+			for(int j=0;j<name.length();j++) { 
+				if(name.charAt(i)==name.charAt(j)) { 
+					count++;
+			//		freq[i]=count;
+				}	
+				
+			}
+			//System.out.println(ch+" appers "+count+" Times");
+			letters[i]=name.charAt(i);
+			freq[i]=count;
+			for(int j=i+1;j<name.length();j++) { 
+				if(name.charAt(i)==name.charAt(j)) { 
+					//count++;
+					freq[i]=-5;
+				}	
+				
+			}
+			
+		}
+		
+		for(int i=0;i<name.length();i++) { 
+			
+				 if(freq[i]>0 && letters[i]!=' ') { 
+					 System.out.println(letters[i]+" Appers "+freq[i]+" Times");
+				 }
+			
+		}
+
+}
+}
+/* output :
+ R Appers 1 Times
+J Appers 1 Times
+N Appers 2 Times
+A Appers 3 Times
+B Appers 2 Times
+U Appers 1 Times
+*/
