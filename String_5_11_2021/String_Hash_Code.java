c122644551144e5f94df8ac49e217d73364e2f7e
santhiya8884 <sanammu91@gmail.com>
2021-11-05 10:01:43 +0530

    String_Hash_Code_Demo

---
+++ String_5_11_2021/String_Hash_Code.java
@@ -0,0 +1,26 @@
+package string_5_11_2021;
+
+public class String_Hash_Code {
+
+	public String_Hash_Code(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s=new String("santhiya");
+		System.out.println(s);
+		System.out.println(s.hashCode());
+		String_Hash_Code obj=new String_Hash_Code("Hai");
+		System.out.println(obj);
+		System.out.println(s);
+	
+	}
+
+}
+/* output :
+santhiya
+2007774813
+string_5_11_2021.String_Hash_Code@6ff3c5b5
+santhiya
+*/
