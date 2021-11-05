5d244138cdf32c3ac38af9244169e67d6f004475
santhiya8884 <sanammu91@gmail.com>
2021-11-05 21:10:01 +0530

    Count of Characters

---
+++ String_5_11_2021/String_Count_Of_Character.java
@@ -0,0 +1,20 @@
+package string_5_11_2021;
+
+public class String_Count_Of_Character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String_Count_Of_Character sara=new String_Count_Of_Character();
+		sara.countOfCharacter();
+	}
+
+	public void countOfCharacter() {
+		// TODO Auto-generated method stub
+		String obj="Santhiya";
+		System.out.println("Count Of Character "+obj.length());
+	}
+
+}
+/* output :
+Count Of Character 8
+*/
